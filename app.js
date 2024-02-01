ba3adb3cba9c8b98c75e9dcc0e1e442c6faaa7e8
mohamed housni <mhousni@mohameds-MacBook-Pro.local>
2024-02-01 13:27:40 +0100

    added q3 mh

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const q1BtnYes = document.querySelector(".q1-btn-yes");
 const q1BtnNo = document.querySelector(".q1-btn-no");
 const q2BtnYes = document.querySelector(".q2-btn-yes");
 const q2BtnNo = document.querySelector(".q2-btn-no");
+const q3BtnYes = document.querySelector(".q3-btn-yes");
+const q3BtnNo = document.querySelector(".q3-btn-no");
+
 const question3 = document.getElementById("question3");
 const question2 = document.getElementById("question2");
 const question1 = document.getElementById("question1");
@@ -9,18 +12,46 @@ let choiceUnder = true;
 let choiceMedical = true;
 const q2Btn = document.querySelector("#q2");
 const q1Btn = document.querySelector("#q1");
+const q3Btn = document.querySelector("#q3");
 
 q1Btn.style.display = "none";
 q2Btn.style.display = "none";
+q3Btn.style.display = "none";
 document.getElementById("qualify").style.display = "none";
 document.getElementById("dontqualify").style.display = "none";
 
 const startToQualify = document.querySelector("#btn-tap-to");
 
+
+var timeLimitInMinutes = 4;
+var timeLimitInSeconds = timeLimitInMinutes * 60;
+var timerElement = document.getElementById('timer');
+
+function startTimer() {
+    timeLimitInSeconds--;
+    var minutes = Math.floor(timeLimitInSeconds / 60);
+    var seconds = timeLimitInSeconds % 60;
+  
+    if (timeLimitInSeconds < 0) {
+      timerElement.textContent = '00:00';
+      clearInterval(timerInterval);
+      return;
+    }
+  
+    if (minutes < 10) {
+      minutes = '0' + minutes;
+    }
+    if (seconds < 10) {
+      seconds = '0' + seconds;
+    }
+  
+    timerElement.textContent = minutes + ':' + seconds;
+  }
+
 const showText1 = () => {
     console.log(choiceMedical , choiceUnder);
     if(choiceMedical == false && choiceUnder == true ) {
-        // var timerInterval = setInterval(startTimer, 1000);
+        var timerInterval = setInterval(startTimer, 1000);
         document.getElementById("qualify").style.display = "block";
     }else {
         document.getElementById("dontqualify").style.display = "block";
@@ -45,11 +76,23 @@ q1BtnNo.addEventListener("click", () => {
 });
 q2BtnYes.addEventListener("click", () => {
     q2Btn.style.display = "none";
+    q3Btn.style.display = "flex";
     choiceMedical = true;
-    showText1();
+    // showText1();
 });
 q2BtnNo.addEventListener("click", () => {
     q2Btn.style.display = "none";
+    q3Btn.style.display = "flex";
+    choiceMedical = false;
+    // showText1();
+});
+q3BtnYes.addEventListener("click", () => {
+    q3Btn.style.display = "none";
+    choiceMedical = true;
+    showText1();
+});
+q3BtnNo.addEventListener("click", () => {
+    q3Btn.style.display = "none";
     choiceMedical = false;
     showText1();
 });
